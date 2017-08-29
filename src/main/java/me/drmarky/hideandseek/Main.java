9948d7434c11dfe05175fa7a572864365bb80edb
DrMarky <minesoccer315@gmail.com>
2017-08-29 17:03:44 -0400

    initial commit

---
+++ src/main/java/me/drmarky/hideandseek/Main.java
@@ -0,0 +1,37 @@
+package me.drmarky.hideandseek;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        PluginDescriptionFile pdfFile = getDescription();
+        Logger logger = getLogger();
+
+        logger.info(pdfFile.getName() + " has been enabled. (V." + pdfFile.getVersion() + ")");
+
+        registerEvents();
+
+        //new HideAndSeekCommand(this, new RegisterPlayers(new StartGame(this, new StopGame())), new StopGame());
+    }
+
+    @Override
+    public void onDisable() {
+        PluginDescriptionFile pdfFile = getDescription();
+        Logger logger = getLogger();
+
+        logger.info(pdfFile.getName() + " has been disabled. (V." + pdfFile.getVersion() + ")");
+    }
+
+    private void registerEvents() {
+        PluginManager pm = getServer().getPluginManager();
+        //pm.registerEvents(new PlayerMoveListener(), this);
+    }
+
+}
